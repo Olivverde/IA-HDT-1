3ff811e5fe878eef55ba27cd4910a195b30eaac9
Olivverde <dele19270@uvg.edu.gt>
2022-02-07 11:37:21 -0600

    First Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+def toBytes(self):
+    self.r = int(max(min(self.r, 255), 0))
+    self.g = int(max(min(self.g, 255), 0))
+    self.b = int(max(min(self.b, 255), 0))
+    return bytes([self.b, self.g, self.r])
+
+def writebmp(filename, width, height, pixels):
+  f = open(filename, 'bw')
+
+  f.write(char('B'))
+  f.write(char('M'))
+  f.write(dword(14 + 40 + width * height * 3))
+  f.write(dword(0))
+  f.write(dword(14 + 40))
+
+  f.write(dword(40))
+  f.write(dword(width))
+  f.write(dword(height))
+  f.write(word(1))
+  f.write(word(24))
+  f.write(dword(0))
+  f.write(dword(width * height * 3))
+  f.write(dword(0))
+  f.write(dword(0))
+  f.write(dword(0))
+  f.write(dword(0))
+
+  for x in range(height):
+    for y in range(width):
+      f.write(pixels[x][y].toBytes())
+  f.close()
