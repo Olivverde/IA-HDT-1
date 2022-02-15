a1072a3009b24e6fdb99df316ec3aa75a2731024
Olivverde <dele19270@uvg.edu.gt>
2022-02-14 20:06:36 -0600

    Still debugging discretization

--- rw-lib.py
+++ rw-lib.py
@@ -64,10 +64,20 @@ class Writebmp(object):
         f.write(pixels[x][y])
     f.close()
 
+
+
 class Readbmp(object):
     def __init__(self, path):
         self.path = path
+        self.start = self.color(254,0,0)
+        self.availablePath = self.color(255,255,255)
+        self.wall = self.color(0,0,0)
+        self.goal = self.color(5,252,6)
+        self.fraction = 20
+
         self.read()
+        self.discretization()
+        
     
     def color(self, r, g, b): # generador de colores
       return bytes([r, g, b])
@@ -98,6 +108,82 @@ class Readbmp(object):
               self.processed = []
         
         return self.buffer
+    
+    def stadistics(self, pathFlag, frontier, indexes):
+      color = self.color(1, 1, 1)
+      totalPixels = self.pixelRange**2
+      proportion = pathFlag/totalPixels
+      i, j, xRange, yRange = indexes
+      print(proportion)
+      if proportion >= 0.5 and frontier == 0:
+        color = self.availablePath
+      elif proportion < 0.5 and frontier == 0:
+        color = self.wall
+      elif frontier > 0:
+        color = self.start
+     
+      for a in range(i, xRange):
+        for b in range(j, yRange):
+          self.buffer[a][b] = color
+          #print(a,b,i,j)
+      #input('[ENTER]')
+      self.buffer[0][29]= self.start
+
+
+    def discretization(self):
+      bff = self.buffer
+      flagPath, extraFlag = 0, 0
+      self.i, self.j, self.x, self.y = 0, 0, 0, 0
+      self.pixelRange = round(self.width/self.fraction)
+      self.xFraction, self.yFraction = self.fraction, self.fraction
+      self.xRange, self.yRange = self.pixelRange, self.pixelRange
+
+      for x in range(self.x, self.xFraction):
+        for y in range(self.y, self.yFraction):
+          for i in range(self.i, self.xRange):
+            for j in range(self.j, self.yRange):
+              if bff[i][j] == self.availablePath:
+                flagPath += 1
+              elif bff[i][j] == self.goal or bff[i][j] == self.start:
+                extraFlag += 1
+              #print(x,y,i,j)
+          #input('[ENTER]')
+          self.stadistics(flagPath, extraFlag, [self.i, self.j, self.xRange, self.yRange])
+          flagPath, extraFlag = 0,0
+
+          self.j += self.pixelRange
+          self.yRange += self.pixelRange
+        
+        self.j, self.yRange = 0, self.pixelRange
+        self.i += self.pixelRange
+        self.xRange += self.pixelRange
+
+        
+    
+    """
+    def getFrontierRadio(self):
+      self.radio = 0
+      indexCounter = []
+      frontierColor = self.color(254,0,0)
+      offsetColor = self.color(255,255,255)
+      flag = 0
+
+      for i in range(len(self.buffer)):
+        for pixel in range(len(self.buffer[i])):
+          if self.buffer[i][pixel] == frontierColor and flag < 1:
+            flag += 1
+            self.radio = -pixel
+          elif self.buffer[i][pixel] == offsetColor and flag == 1:
+            flag += 1
+            self.radio += pixel
+            indexCounter.append(self.radio)
+        self.radio = 0
+        flag = 0
+      self.radio = max(indexCounter)
+
+      return self.radio
+    """
+
           
 """
 TESTING
@@ -106,7 +192,8 @@ reader = Readbmp("Test2.bmp")
 writer = Writebmp()
 width, height = reader.width, reader.height
 print(width, height)
-print(reader.buffer)
-"""
 writer.writebmp("bmp-out.bmp", width, height, reader.buffer)
+"""
+print(reader.getFrontierRadio())
+
 """
